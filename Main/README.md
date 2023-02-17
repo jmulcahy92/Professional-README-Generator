7f7623bf99a99bf5d99da516f72b6fc7e24bbb84
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-02-16 23:46:44 -0600

    restructure

---
+++ Main/README.md
@@ -0,0 +1,23 @@
+# test1
+![License](https://img.shields.io/badge/)
+## Description
+test2
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+test3
+## Usage
+test4
+## License
+undefined
+## Contributing
+test5
+## Tests
+test6
+## Questions
+If you have additional questions, my GitHub profile is [here](https://www.github.com/test7). My email address is test8
--- Main/utils/generateMarkdown.js
+++ Main/utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  return '![License](https://img.shields.io/badge/)';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +15,8 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# <${data.title}>
-![License](https://img.shields.io/badge/)
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
 ## Description
 ${data.desc}
 ## Table of Contents
@@ -28,13 +31,13 @@ ${data.install}
 ## Usage
 ${data.usage}
 ## License
-
+${renderLicenseSection(data.license)}
 ## Contributing
 ${data.contributing}
 ## Tests
 ${data.tests}
 ## Questions
-If you have additional questions, my GitHub profile is at ${data.github}. My email address is ${data.email}
+If you have additional questions, my GitHub profile is [here](https://www.github.com/${data.github}). My email address is ${data.email}
 `;
 }
 
