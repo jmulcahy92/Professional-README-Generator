a2fecb27a1fe8c6dda640f7715d551b4660fc180
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-02-16 21:59:24 -0600

    npm init and install inquirer, require packages, basic structure

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, '', (err) => err ? console.error(err) : console.log('Commit logged!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => writeToFile("README.md", response));
+}
 
 // Function call to initialize app
 init();
